e658d42fc8a4640ae93176886dbf3c3048d22c33
kujira12 <ha9botan@gmail.com>
2024-08-06 16:04:38 +0900

    roles　編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -51,7 +51,7 @@
 # ------------------------------------
 server "35.75.15.226",
   user: "ec2-user",
-  roles: %w{web app},
+  roles: %w{web db app},
   ssh_options: {
     user: "ec2-user", # overrides user setting above
     keys: %w(~/.ssh/dic_sample.pem),
