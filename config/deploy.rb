14fc440febfee18436727816b7269444093e1660
kujira12 <ha9botan@gmail.com>
2024-08-06 15:59:29 +0900

    deploy.rbモジュールを読み込む設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,32 @@ set :repo_url, "https://github.com/okamoto413/AWS__Capistrano_blog_app.git"
 set :bundle_without, %w{test}.join(':')
 
 set :rbenv_version, '3.3.0'   # 追記
-append :linked_files, 'config/secrets.yml'
+set :linked_files, %w{config/secrets.yml}   # 4
+
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}   # 5
+ set :keep_releases, 5   # 6
+set :rbenv_ruby, '3.3.0'    # 7
+ set :log_level, :info   # 8
+
+ after 'deploy:published', 'deploy:seed'   # 9
+ after 'deploy:finished', 'deploy:restart'   # 10
+
+ namespace :deploy do
+   desc 'Run seed'
+   task :seed do
+     on roles(:db) do
+       with rails_env: fetch(:rails_env) do
+         within current_path do
+           execute :bundle, :exec, :rake, 'db:seed'
+         end
+       end
+     end
+   end
+   desc 'Restart application'
+   task :restart do
+     invoke 'unicorn:restart'
+  end
+ end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
